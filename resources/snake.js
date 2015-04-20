66856fcde1a3bc17301e42530f7c476cac97889e
Ben Vreeland <vreelb@gmail.com>
2015-04-19 21:02:09 -0400

    Initial file setup.

---
+++ resources/snake.js
@@ -0,0 +1,10 @@
+function gameSetup() {
+	var game_canvas = document.getElementById("game");
+	var context = game_canvas.getContext("2d");
+	
+}
+
+
+$(document).ready(function() {
+	gameSetup();
+}
