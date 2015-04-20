10fc04d6aa105a04682bfe14287682d2a71fc888
Ben Vreeland <vreelb@gmail.com>
2015-04-20 14:16:24 -0400

    It's working, it's working!
    Now this is podracing.
    Next I'll actually start optimizing and improving things, but my first
    canvas game is a thing now.

--- resources/snake.js
+++ resources/snake.js
@@ -1,6 +1,6 @@
 function getRandomLocation(pixel_size, blacklist) {	
 	var x = Math.floor(Math.random() * ($("#game-canvas").width()/pixel_size) );
-	var y = Math.floor(Math.random() * ($("#game-canvas").width()/pixel_size) );
+	var y = Math.floor(Math.random() * ($("#game-canvas").height()/pixel_size) );
 	
 	var x_y = [x, y];
 	//console.log(x_y);
@@ -17,6 +17,7 @@ function getRandomLocation(pixel_size, blacklist) {
 
 function drawGame(game) {
 	var px = game.pixel_size;
+	game.context.clearRect(0, 0, $("#game-canvas").width(), $("#game-canvas").height());
 	
 	var snake_pic = new Image();
 	snake_pic.src = game.snake.pic;
@@ -25,16 +26,61 @@ function drawGame(game) {
 	
 	food_pic.onload = function() {
 		game.context.drawImage(food_pic, game.food.loc[0]*px, game.food.loc[1]*px, px, px);
-		console.log("food drawn at "+game.food.loc[0]+", "+game.food.loc[1]);
+		//console.log("food drawn at "+game.food.loc[0]+", "+game.food.loc[1]);
 	}
 	
 	snake_pic.onload = function() {
-		for (var hump of game.snake.loc) {
-			game.context.drawImage(snake_pic, hump[0]*px, hump[1]*px, px, px);
-			console.log("snake piece drawn at "+hump);
+		for (var node of game.snake.loc) {
+			game.context.drawImage(snake_pic, node[0]*px, node[1]*px, px, px);
+			//console.log("snake piece drawn at "+node);
 		}
 	}
 	
+}
+
+function tick(game) {
+	var flag = 0;
+	//test for next location
+		//if wall or snake, dead
+		//if empty or food, move snake
+			//if food, increase score and find new food location
+	var next_loc = [game.snake.loc[0][0], game.snake.loc[0][1]];
+	
+	if (game.snake.direction == "n") {
+		next_loc[1]--;
+	} else if (game.snake.direction == "s") {
+		next_loc[1]++;
+	} else if (game.snake.direction == "e") {
+		next_loc[0]++;
+	} else if (game.snake.direction == "w") {
+		next_loc[0]--;
+	}
+	console.log(game.snake.direction+" moving towards "+next_loc);
+	
+	if ((next_loc[0] < 0)||(next_loc[1] < 0)) { // does it hit a wall?
+		console.log("LOSS: snake hit wall\nSCORE: "+ game.score);
+		return -1;
+	} else {
+		for (nongrata of game.snake.loc) {		// does it hit itself?
+			if ((next_loc[0] === nongrata[0])&&(next_loc[1] === nongrata[1])) {
+				console.log("LOSS: snake hit itself\nSCORE: "+ game.score);
+				return -1;
+			}
+		}
+	}
+	
+	game.snake.loc.unshift(next_loc);
+	
+	if ((next_loc[0] === game.food.loc[0])&&(next_loc[1] === game.food.loc[1])) {
+		game.score++;
+		
+		game.food.loc = getRandomLocation(game.pixel_size, game.snake.loc);
+	} else {
+		game.snake.loc.pop();
+	}
+	
+	//console.log(game.tick++, game.snake.loc, game.score);
+	return flag;
 }
 
 function runGame(scale) {
@@ -44,21 +90,59 @@ function runGame(scale) {
 		context: game_canvas.getContext("2d"),
 		pixel_size: $("#game-canvas").width()/scale,
 		score: 0,
+		tick: 0,
 		snake: {
-			pic: "test.png",
-			length: 5,
-			direction: Math.floor(Math.random()*4),		// clockwise N, E, S, W
+			pic: "https://i.imgur.com/CnZ7qW3.png",//"test.png",
+			direction: "w",
 			loc: [[5,5],[6,5],[7,5],[7,6]],
 		},
 		food: {
-			pic: "test2.png",
+			pic: "https://i.imgur.com/grps5vJ.png",//"test2.png",
 			loc: [],
 		},
 	};
 	
 	game.food.loc = getRandomLocation(game.pixel_size, game.snake.loc);
 	
-	drawGame(game);
+	$(document).keydown(function(e) {
+	switch(e.which) {
+		case 38: // up
+		game.snake.direction = "n";
+		break;
+		case 37: // left
+		game.snake.direction = "w";
+		break;
+		case 40: // down
+		game.snake.direction = "s";
+		break;
+		case 39: // right
+		game.snake.direction = "e";
+		break;
+
+		case 87: // w
+		game.snake.direction = "n";
+		break;
+		case 65: // a
+		game.snake.direction = "w";
+		break;
+		case 83: // s
+		game.snake.direction = "s";
+		break;
+		case 68: // d
+		game.snake.direction = "e";
+		break;
+
+		default: return;
+	}
+	e.preventDefault();
+	});
+	
+	var game_timer = setInterval( function() {
+		if (tick(game) < 0) {
+			clearInterval(game_timer);
+		}
+		drawGame(game);
+	}, 1000);
 	
 }
 
